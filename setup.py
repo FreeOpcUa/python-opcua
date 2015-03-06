35bd771ce92cc0b17e0e4b286aaaea603ac746dc
Olivier R-D <olivier.roulet@gmail.com>
2015-03-06 12:13:14 +0100

    add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from distutils.core import setup
+from distutils.command.install_data import install_data
+
+
+setup (name = "freeopcua", 
+        version = "0.5.1",
+        description = "Pure Python OPC-UA client library",
+        author = "Olivier Roulet-Dubonnet",
+        author_email = "olivier.roulet@gmail.com",
+        url = 'http://freeopcua.github.io/',
+        packages = ["opcua"],
+        provides = ["opcua"],
+        license = "GNU General Public License v3",
+
+        classifiers = [
+            "Programming Language :: Python",
+            "Programming Language :: Python :: 3",
+            "Development Status :: 4 - Beta",
+            "Intended Audience :: Developers",
+            "Operating System :: OS Independent",
+            "Topic :: Software Development :: Libraries :: Python Modules",
+        ]
+        )
+
+
