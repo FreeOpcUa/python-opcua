79f02f7e32109a242c7afe8f94409c10e4066898
Olivier R-D <olivier.roulet@gmail.com>
2015-07-07 21:08:50 +0200

    add missing users.py file!!!

---
+++ opcua/users.py
@@ -0,0 +1,15 @@
+"""
+Implement user managent here
+"""
+
+from enum import Enum
+class User(Enum):
+    """
+    Define some default users.
+    """
+    Admin = 0
+    Anonymous = 1
+    User = 3
+
+
+
