00e63b43406b160f17d23a0a3e24cd6d6fc302ac
Olivier R-D <olivier@loke>
2015-09-09 09:55:28 +0200

    write dependencies in setup.py for python2 (opcua for python3 has no dependencies)

--- setup.py
+++ setup.py
@@ -1,9 +1,16 @@
 from distutils.core import setup
 from distutils.command.install_data import install_data
 
+import sys
 
-setup (name = "freeopcua", 
-        version = "0.9.7",
+if sys.version_info[0] < 3:
+    install_requires = ["enum3.4", "trollius", "futures"]
+else:
+    install_requires = []
+
+setup(
+        name = "freeopcua", 
+        version = "0.9.8",
         description = "Pure Python OPC-UA client and server library",
         author = "Olivier Roulet-Dubonnet",
         author_email = "olivier.roulet@gmail.com",
@@ -11,15 +18,16 @@ setup (name = "freeopcua",
         packages = ["opcua"],
         provides = ["opcua"],
         license = "GNU Lesser General Public License",
-
+        install_requires = install_requires,
         classifiers = [
             "Programming Language :: Python",
             "Programming Language :: Python :: 3",
+            "Programming Language :: Python :: 2",
             "Development Status :: 4 - Beta",
             "Intended Audience :: Developers",
             "Operating System :: OS Independent",
             "Topic :: Software Development :: Libraries :: Python Modules",
         ]
-        )
+    )
 
 
