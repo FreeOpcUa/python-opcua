6a85beb76de19aafaed9450d1728f2d88bb7c9ec
olivier R-D <olivier@loke>
2015-12-10 13:01:27 +0100

    new release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
     install_requires = ["pycrypto"]
 
 setup(name="freeopcua", 
-      version="0.9.18",
+      version="0.9.19",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
