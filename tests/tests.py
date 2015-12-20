825c6e27cf61da8b4c8f53e1e4494e4e25f5f82e
olivier R-D <olivier@loke>
2015-12-20 19:42:59 +0100

    more command line tests

--- tests/tests.py
+++ tests/tests.py
@@ -937,6 +937,7 @@ class TestCmdLines(unittest.TestCase):
         obj = objects.add_object(4, "directory")
         var = obj.add_variable(4, "variable", 1.999)
         var2 = obj.add_variable(4, "variable2", 1.777)
+        var2.set_writable()
         self.srv.start()
 
     def test_uals(self):
@@ -948,9 +949,20 @@ class TestCmdLines(unittest.TestCase):
         self.assertIn(b"1.999", s)
 
     def test_uaread(self):
+        s = subprocess.check_output(["python3", "tools/uaread", "--url", self.srv_url, "--path", "0:Objects,4:directory,4:variable"])
+        self.assertIn(b"1.999", s)
+
+    def test_uawrite(self):
+        s = subprocess.check_output(["python3", "tools/uawrite", "--url", self.srv_url, "--path", "0:Objects,4:directory,4:variable2", "1.789"])
         s = subprocess.check_output(["python3", "tools/uaread", "--url", self.srv_url, "--path", "0:Objects,4:directory,4:variable2"])
-        self.assertIn(b"1.777", s)
+        self.assertIn(b"1.789", s)
         self.assertNotIn(b"1.999", s)
+
+    def test_uadiscover(self):
+        s = subprocess.check_output(["python3", "tools/uadiscover", "--url", self.srv_url])
+        self.assertIn(b"opc.tcp://localhost", s)
+        self.assertIn(b"FreeOpcUa", s)
+        self.assertIn(b"urn:freeopcua:python:server", s)
 
     @classmethod
     def tearDownClass(self):
