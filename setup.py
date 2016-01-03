a67463e4c9cd48acf49ef6c9b17867a7e272b4e8
olivier R-D <olivier@loke>
2016-01-03 10:54:31 +0100

    new release

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
     install_requires = []
 
 setup(name="freeopcua", 
-      version="0.10.1",
+      version="0.10.2",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
