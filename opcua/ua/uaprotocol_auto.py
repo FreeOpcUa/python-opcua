97f75d39ece3511eda4fed0f45e29e118ffddf00
olivier R-D <olivier@loke>
2016-01-18 11:41:15 +0100

    support for empty ExtensionObjects

--- opcua/ua/uaprotocol_auto.py
+++ opcua/ua/uaprotocol_auto.py
@@ -14347,7 +14347,8 @@ def extensionobject_from_binary(data):
         e = ExtensionObject()
         e.TypeId = TypeId
         e.Encoding = Encoding
-        e.Body = body.read(len(body))
+        if body is not None:
+            e.Body = body.read(len(body))
         return e
     klass = ExtensionClasses[TypeId.Identifier]
     if body is None:
