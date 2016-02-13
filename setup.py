8e3c7882a72da4c0d14d8a901b53d443e2030ee5
Zhang, Hua <zhanghua@sjtubbs.org>
2016-02-13 15:50:50 +0800

    setup.py translate trollius source code to asyncio style during installation
    add a commmand asyncio_translate to setup.py to help development switch between py2 and py3.
    call example: ./setup.py asyncio_translate

--- setup.py
+++ setup.py
@@ -1,13 +1,46 @@
 from setuptools import setup, find_packages
 
 import sys
+import re
+from os import path
 
-if sys.version_info[0] < 3:
-    install_requires = ["enum34", "trollius", "futures"]
+
+def translate_from_trollius_to_asyncio():
+    pypaths = [
+        ["client", "ua_client.py"],
+    ]
+
+    curdir = path.dirname(path.realpath(__file__))
+    for pypath in pypaths:
+        fpath = path.join(curdir, "opcua", *pypath)
+        with open(fpath, 'r+b') as f:
+            src = f.read()
+            src = re.sub(br"raise Return\(\s*\)", b"return", src)
+            src = re.sub(br"raise Return\(", b"return (", src)
+            src = re.sub(br"yield From\(None\)", b"yield from []", src)
+            src = re.sub(br"yield From\(", b"yield from (", src)
+            f.seek(0)
+            f.truncate(0)
+            f.write(src)
+
+if len(sys.argv) >= 2 and sys.argv[1] == "asyncio_translate":
+    translate_from_trollius_to_asyncio()
+    sys.exit()
+
+install_requires = []
+if sys.version_info[0] < 3 or sys.version_info[1] < 2:
+    # python 2.7 - python 3.1
+    install_requires.append("futures")
+if sys.version_info[0] < 3 or sys.version_info[1] < 3:
+    # python 2.7 - python 3.2
+    install_requires.append("trollius")
 else:
-    install_requires = []
+    translate_from_trollius_to_asyncio()
+if sys.version_info[0] < 3 or sys.version_info[1] < 4:
+    # python 2.7 - python 3.3
+    install_requires.append("enum34")
 
-setup(name="freeopcua", 
+setup(name="freeopcua",
       version="0.10.6",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
@@ -29,7 +62,7 @@ setup(name="freeopcua",
                    "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
                    "Topic :: Software Development :: Libraries :: Python Modules",
                    ],
-      entry_points={'console_scripts': 
+      entry_points={'console_scripts':
                     [
                         'uaread = opcua.tools:uaread',
                         'uals = opcua.tools:uals',
@@ -43,4 +76,3 @@ setup(name="freeopcua",
                     ]
                     }
       )
-
