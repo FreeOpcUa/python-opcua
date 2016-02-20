2c56efe690f1cb43fb4a8a2e7901e476f75cf3f2
olivier R-D <olivier@loke>
2016-02-20 08:22:55 +0100

    disable crypto tests f crypto not available

--- tests/tests.py
+++ tests/tests.py
@@ -3,6 +3,13 @@ import logging
 import sys
 sys.path.insert(0, "..")
 sys.path.insert(0, ".")
+try:
+    from opcua.crypto import uacrypto
+    CRYPTOGRAPHY_AVAILABLE = True
+except ImportError:
+    print("WARNING: CRYPTO NOT AVAILABLE, CRYPTO TESTS DISABLED!!")
+    CRYPTOGRAPHY_AVAILABLE = False
+
 
 
 
@@ -10,7 +17,8 @@ from tests_cmd_lines import TestCmdLines
 from tests_server import TestServer
 from tests_client import TestClient
 from tests_unit import Unit
-from tests_crypto_connect import TestCryptoConnect
+if CRYPTOGRAPHY_AVAILABLE:
+    from tests_crypto_connect import TestCryptoConnect
 
 
 if __name__ == '__main__':
