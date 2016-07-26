1cbc69487534864a58230ebe15688c51b3d83e5a
georgschoelly <git@gsch.ch>
2016-07-26 23:17:04 +0200

    replace custom test disabling with unittest.skip (#249)
    Instead of using our own solution for skipping test, we use
    unittest.skip. This has the additional benefit that it works
    with the automatic test discovery in Python >=3.2.

--- tests/tests.py
+++ tests/tests.py
@@ -3,14 +3,6 @@ import logging
 import sys
 sys.path.insert(0, "..")
 sys.path.insert(0, ".")
-try:
-    from opcua.crypto import uacrypto
-    CRYPTOGRAPHY_AVAILABLE = True
-except ImportError:
-    print("WARNING: CRYPTO NOT AVAILABLE, CRYPTO TESTS DISABLED!!")
-    CRYPTOGRAPHY_AVAILABLE = False
-
-
 
 
 from tests_cmd_lines import TestCmdLines
@@ -19,8 +11,7 @@ from tests_client import TestClient
 from tests_unit import TestUnit
 from tests_history import TestHistory, TestHistorySQL, TestHistoryLimits, TestHistorySQLLimits
 from tests_history import TestHistorySQL
-if CRYPTOGRAPHY_AVAILABLE:
-    from tests_crypto_connect import TestCryptoConnect
+from tests_crypto_connect import TestCryptoConnect
 
 
 if __name__ == '__main__':
