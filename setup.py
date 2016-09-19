84f73bd52ecf74ab1f5fda10d4acf09e8f5e43da
olivier R-D <olivier@loke>
2016-09-19 20:01:03 +0200

    new release

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ else:
     install_requires = ["python-dateutil"]
 
 setup(name="freeopcua", 
-      version="0.10.14",
+      version="0.10.15",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
