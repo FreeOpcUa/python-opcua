6a4d353cdac0c1b1ae4a25642d5572d988963137
Mathias Lüdtke <mathias.luedtke@ipa.fraunhofer.de>
2017-11-24 14:18:35 +0100

    introduced call_method_full
    outputs the full CallMethodResult object instead of the OutputArguments only

--- opcua/common/methods.py
+++ opcua/common/methods.py
@@ -12,28 +12,34 @@ def call_method(parent, methodid, *args):
     nodeid of method as a NodeId object
     arguments are variants or python object convertible to variants.
     which may be of different types
-    returns a list of variants which are output of the method
+    returns a list of values or a single value depending on the output of the method
+    """
+    result = call_method_full(parent, methodid, *args)
+
+    if len(result.OutputArguments) == 0:
+        return None
+    elif len(result.OutputArguments) == 1:
+        return result.OutputArguments[0]
+    else:
+        return result.OutputArguments
+
+
+def call_method_full(parent, methodid, *args):
+    """
+    Call an OPC-UA method. methodid is browse name of child method or the
+    nodeid of method as a NodeId object
+    arguments are variants or python object convertible to variants.
+    which may be of different types
+    returns a CallMethodResult object with converted OutputArguments
     """
     if isinstance(methodid, (str, ua.uatypes.QualifiedName)):
         methodid = parent.get_child(methodid).nodeid
     elif isinstance(methodid, node.Node):
         methodid = methodid.nodeid
 
-    arguments = []
-    for arg in args:
-        if not isinstance(arg, ua.Variant):
-            arg = ua.Variant(arg)
-        arguments.append(arg)
-
-    result = _call_method(parent.server, parent.nodeid, methodid, arguments)
-
-    if len(result.OutputArguments) == 0:
-        return None
-    elif len(result.OutputArguments) == 1:
-        return result.OutputArguments[0].Value
-    else:
-        return [var.Value for var in result.OutputArguments]
-
+    result = _call_method(parent.server, parent.nodeid, methodid, to_variant(*args))
+    result.OutputArguments = [var.Value for var in result.OutputArguments]
+    return result
 
 def _call_method(server, parentnodeid, methodid, arguments):
     request = ua.CallMethodRequest()
