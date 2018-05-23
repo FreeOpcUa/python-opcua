06f89a20ada9bc766810b964fdf89e55275bbb67
jobasto <35599890+jobasto@users.noreply.github.com>
2018-05-23 13:39:38 +0200

    Add tests for structs containing enums

--- tests/tests_client.py
+++ tests/tests_client.py
@@ -8,6 +8,8 @@ from tests_subscriptions import SubscriptionTests
 from tests_common import CommonTests, add_server_methods
 from tests_xml import XmlTests
 
+from tests_enum_struct import add_server_custom_enum_struct
+
 port_num1 = 48510
 
 
@@ -24,6 +26,7 @@ class TestClient(unittest.TestCase, CommonTests, SubscriptionTests, XmlTests):
         cls.srv = Server()
         cls.srv.set_endpoint('opc.tcp://127.0.0.1:{0:d}'.format(port_num1))
         add_server_methods(cls.srv)
+        add_server_custom_enum_struct(cls.srv)
         cls.srv.start()
 
         # start admin client
@@ -108,4 +111,12 @@ class TestClient(unittest.TestCase, CommonTests, SubscriptionTests, XmlTests):
         nenumstrings = self.opc.get_node(ua.ObjectIds.AxisScaleEnumeration_EnumStrings)
         with self.assertNotRaises(Exception):
             value = ua.Variant(nenumstrings.get_value())
+            
+    def test_custom_enum_struct(self):
+        self.ro_clt.load_type_definitions()
+        ns = self.ro_clt.get_namespace_index('http://yourorganisation.org/struct_enum_example/')
+        myvar = self.ro_clt.get_node(ua.NodeId(6009, ns))
+        val = myvar.get_value()
+        self.assertEqual(val.IntVal1, 242)
+        self.assertEqual(val.EnumVal, ua.ExampleEnum.EnumVal2)
 
