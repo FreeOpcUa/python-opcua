e3eb5c8a0a0445f4686e984f85a273fc3092f0ce
oroulet <olivier@loke>
2018-06-04 18:38:46 +0200

    add a very simple class to update a variable in example-server

--- examples/server-example.py
+++ examples/server-example.py
@@ -1,7 +1,11 @@
+from threading import Thread
 import sys
 sys.path.insert(0, "..")
 import logging
 from datetime import datetime
+import time
+from math import sin
+from threading import Timer
 
 try:
     from IPython import embed
@@ -47,6 +51,23 @@ def func(parent, variant):
 def multiply(parent, x, y):
     print("multiply method call with parameters: ", x, y)
     return x * y
+
+
+class VarUpdater(Thread):
+    def __init__(self, var):
+        Thread.__init__(self)
+        self._stopev = False
+        self.var = var
+
+    def stop(self):
+        self._stopev = True
+
+    def run(self):
+        while not self._stopev:
+            v = sin(time.time() / 10)
+            self.var.set_value(v)
+            time.sleep(0.1)
+
 
 
 if __name__ == "__main__":
@@ -96,6 +117,7 @@ if __name__ == "__main__":
     # create directly some objects and variables
     myobj = server.nodes.objects.add_object(idx, "MyObject")
     myvar = myobj.add_variable(idx, "MyVariable", 6.7)
+    mysin = myobj.add_variable(idx, "MySin", 0, ua.VariantType.Float)
     myvar.set_writable()    # Set MyVariable to be writable by clients
     mystringvar = myobj.add_variable(idx, "MyStringVariable", "Really nice string")
     mystringvar.set_writable()    # Set MyVariable to be writable by clients
@@ -119,6 +141,8 @@ if __name__ == "__main__":
     # starting!
     server.start()
     print("Available loggers are: ", logging.Logger.manager.loggerDict.keys())
+    vup = VarUpdater(mysin)  # just  a stupide class update a variable
+    vup.start()
     try:
         # enable following if you want to subscribe to nodes on server side
         #handler = SubHandler()
@@ -130,4 +154,5 @@ if __name__ == "__main__":
 
         embed()
     finally:
+        vup.stop()
         server.stop()
