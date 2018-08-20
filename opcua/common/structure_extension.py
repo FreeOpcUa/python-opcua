7dd33f8bfbe2d827d59ff424035e365abe029817
Peiren Yang <yangpeiren@163.com>
2018-08-20 10:24:04 +0200

    refactored two classes

--- opcua/common/structure_extension.py
+++ opcua/common/structure_extension.py
@@ -3,6 +3,9 @@ from opcua import ua
 import xml.etree.ElementTree as Et
 import re
 
+# Indicates which type should be OPC build in types
+_ua_build_in_types = [ua_type for ua_type in ua.VariantType.__members__ if ua_type != 'ExtensionObject']
+
 
 def _repl_func(m):
     """
@@ -26,10 +29,9 @@ def _to_camel_case(name):
 
 class OPCTypeDictionaryBuilder:
 
-    def __init__(self, idx_name, build_in_list):
+    def __init__(self, idx_name):
         """
         :param idx_name: name of the name space
-        :param build_in_list: indicates which type should be build in types,
         types in dict is created as opc:xxx, otherwise as tns:xxx
         """
         head_attributes = {'xmlns:xsi': 'http://www.w3.org/2001/XMLSchema-instance', 'xmlns:tns': idx_name,
@@ -42,22 +44,23 @@ class OPCTypeDictionaryBuilder:
         name_space.attrib['Namespace'] = 'http://opcfoundation.org/UA/'
 
         self._structs_dict = {}
-        self._build_in_list = build_in_list
-
-    def _add_field(self, type_name, variable_name, struct_name):
+        self._build_in_list = _ua_build_in_types
+    
+    def _process_type(self, type_name):
         if type_name in self._build_in_list:
             type_name = 'opc:' + type_name
         else:
             type_name = 'tns:' + _to_camel_case(type_name)
+        return type_name
+
+    def _add_field(self, type_name, variable_name, struct_name):
+        type_name = self._process_type(type_name)
         field = Et.SubElement(self._structs_dict[struct_name], 'opc:Field')
         field.attrib['Name'] = variable_name
         field.attrib['TypeName'] = type_name
 
     def _add_array_field(self, type_name, variable_name, struct_name):
-        if type_name in self._build_in_list:
-            type_name = 'opc:' + type_name
-        else:
-            type_name = 'tns:' + _to_camel_case(type_name)
+        type_name = self._process_type(type_name)
         array_len = 'NoOf' + variable_name
         field = Et.SubElement(self._structs_dict[struct_name], 'opc:Field')
         field.attrib['Name'] = array_len
@@ -102,6 +105,16 @@ class OPCTypeDictionaryBuilder:
                 elem.tail = i
 
 
+def _reference_generator(source_id, target_id, reference_type, is_forward=True):
+    ref = ua.AddReferencesItem()
+    ref.IsForward = is_forward
+    ref.ReferenceTypeId = reference_type
+    ref.SourceNodeId = source_id
+    ref.TargetNodeClass = ua.NodeClass.DataType
+    ref.TargetNodeId = target_id
+    return ref
+
+
 class DataTypeDictionaryBuilder:
 
     def __init__(self, server, idx, idx_name, dict_name):
@@ -111,8 +124,7 @@ class DataTypeDictionaryBuilder:
         # Risk of bugs using a fixed number without checking
         self._id_counter = 8000
         self.dict_id = self._add_dictionary(dict_name)
-        ua_build_in_types = [item for item in ua.VariantType.__members__ if item != 'ExtensionObject']
-        self._type_dictionary = OPCTypeDictionaryBuilder(idx_name, ua_build_in_types)
+        self._type_dictionary = OPCTypeDictionaryBuilder(idx_name)
 
     def nodeid_generator(self):
         self._id_counter += 1
@@ -138,43 +150,33 @@ class DataTypeDictionaryBuilder:
 
         return dictionary_node_id
 
-    @staticmethod
-    def _reference_generator(source_id, target_id, reference_type, is_forward=True):
-        ref = ua.AddReferencesItem()
-        ref.IsForward = is_forward
-        ref.ReferenceTypeId = reference_type
-        ref.SourceNodeId = source_id
-        ref.TargetNodeClass = ua.NodeClass.DataType
-        ref.TargetNodeId = target_id
-        return ref
-
     def _link_nodes(self, linked_obj_node_id, data_type_node_id, description_node_id):
         """link the three node by their node ids according to UA standard"""
         refs = [
                 # add reverse reference to BaseDataType -> Structure
-                self._reference_generator(data_type_node_id, ua.NodeId(ua.ObjectIds.Structure, 0),
-                                          ua.NodeId(ua.ObjectIds.HasSubtype, 0), False),
+                _reference_generator(data_type_node_id, ua.NodeId(ua.ObjectIds.Structure, 0),
+                                     ua.NodeId(ua.ObjectIds.HasSubtype, 0), False),
                 # add reverse reference to created data type
-                self._reference_generator(linked_obj_node_id, data_type_node_id,
-                                          ua.NodeId(ua.ObjectIds.HasEncoding, 0), False),
+                _reference_generator(linked_obj_node_id, data_type_node_id,
+                                     ua.NodeId(ua.ObjectIds.HasEncoding, 0), False),
                 # add HasDescription link to dictionary description
-                self._reference_generator(linked_obj_node_id, description_node_id,
-                                          ua.NodeId(ua.ObjectIds.HasDescription, 0)),
+                _reference_generator(linked_obj_node_id, description_node_id,
+                                     ua.NodeId(ua.ObjectIds.HasDescription, 0)),
                 # add reverse HasDescription link
-                self._reference_generator(description_node_id, linked_obj_node_id,
-                                          ua.NodeId(ua.ObjectIds.HasDescription, 0), False),
+                _reference_generator(description_node_id, linked_obj_node_id,
+                                     ua.NodeId(ua.ObjectIds.HasDescription, 0), False),
                 # add link to the type definition node
-                self._reference_generator(linked_obj_node_id, ua.NodeId(ua.ObjectIds.DataTypeEncodingType, 0),
-                                          ua.NodeId(ua.ObjectIds.HasTypeDefinition, 0)),
+                _reference_generator(linked_obj_node_id, ua.NodeId(ua.ObjectIds.DataTypeEncodingType, 0),
+                                     ua.NodeId(ua.ObjectIds.HasTypeDefinition, 0)),
                 # add has type definition link
-                self._reference_generator(description_node_id, ua.NodeId(ua.ObjectIds.DataTypeDescriptionType, 0),
-                                          ua.NodeId(ua.ObjectIds.HasTypeDefinition, 0)),
+                _reference_generator(description_node_id, ua.NodeId(ua.ObjectIds.DataTypeDescriptionType, 0),
+                                     ua.NodeId(ua.ObjectIds.HasTypeDefinition, 0)),
                 # add forward link of dict to description item
-                self._reference_generator(self.dict_id, description_node_id,
-                                          ua.NodeId(ua.ObjectIds.HasComponent, 0)),
+                _reference_generator(self.dict_id, description_node_id,
+                                     ua.NodeId(ua.ObjectIds.HasComponent, 0)),
                 # add reverse link to dictionary
-                self._reference_generator(description_node_id, self.dict_id,
-                                          ua.NodeId(ua.ObjectIds.HasComponent, 0), False)]
+                _reference_generator(description_node_id, self.dict_id,
+                                     ua.NodeId(ua.ObjectIds.HasComponent, 0), False)]
         self._session_server.add_references(refs)
 
     def create_data_type(self, type_name):
