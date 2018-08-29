4c4f29e609713bc5a0baf5eefe66fe28e9bc8b32
oroulet <olivier@loke>
2018-08-29 10:56:58 +0200

    do not raise crypto error if signature error, raise UaError so it get catched

--- opcua/crypto/uacrypto.py
+++ opcua/crypto/uacrypto.py
@@ -1,7 +1,6 @@
 import os
 
 from cryptography import x509
-from cryptography.exceptions import InvalidSignature
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import serialization
 from cryptography.hazmat.primitives import hashes
