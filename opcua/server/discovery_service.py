56f066e1b31fc5bc77a4c969146c6b0e522b84ff
brubbel <5667690+brubbel@users.noreply.github.com>
2018-11-22 00:08:49 +0100

    discovery/registration services code cleanup

--- opcua/server/discovery_service.py
+++ opcua/server/discovery_service.py
@@ -3,12 +3,14 @@ from opcua import ua
 
 class LocalDiscoveryService(object):
 
-    class ServerDesc(object):
+    class ServerDescription(object):
         def __init__(self, appDesc, uaDiscoveryConfiguration=None):
+            assert(isinstance(appDesc, ua.uaprotocol_auto.ApplicationDescription))
             self.applicationDescription = appDesc
             self.discoveryConfiguration = uaDiscoveryConfiguration
 
     def __init__(self):
+        # _known_servers[applicationUri] = ServerDescription-instance
         self._known_servers = {}
 
     def find_servers(self, params):
@@ -27,6 +29,10 @@ class LocalDiscoveryService(object):
                     break
         return servers
 
+    def add_server_description(self, srvDesc):
+        assert(isinstance(srvDesc, LocalDiscoveryService.ServerDescription))
+        self._known_servers[srvDesc.applicationDescription.ApplicationUri] = srvDesc
+
     def register_server(self, registeredServer, uaDiscoveryConfiguration=None):
         assert(isinstance(registeredServer, ua.uaprotocol_auto.RegisteredServer))
         appDesc = ua.ApplicationDescription()
@@ -38,12 +44,9 @@ class LocalDiscoveryService(object):
         appDesc.DiscoveryUrls = registeredServer.DiscoveryUrls
         # FIXME: select discovery uri using reachability from client network
         appDesc.GatewayServerUri = registeredServer.GatewayServerUri
-        self.add_server_description(appDesc, uaDiscoveryConfiguration)
+        # Create and add ServerDescription, so it is resolved by find_servers().
+        srvDesc = LocalDiscoveryService.ServerDescription(appDesc, uaDiscoveryConfiguration)
+        self.add_server_description(srvDesc)
 
-    def add_server_description(self, appDesc, uaDiscoveryConfiguration=None):
-        assert(isinstance(appDesc, ua.uaprotocol_auto.ApplicationDescription))
-        srvDesc = LocalDiscoveryService.ServerDesc(appDesc, uaDiscoveryConfiguration)
-        self._known_servers[appDesc.ApplicationUri] = srvDesc
-        
     def register_server2(self, params):
         return self.register_server(params.Server, params.DiscoveryConfiguration)
