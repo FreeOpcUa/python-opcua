1cceb101f9f89c41bef9706a1e49d460bf046789
brubbel <5667690+brubbel@users.noreply.github.com>
2018-11-21 08:57:06 +0100

    catch socket errors ENOTCONN/EBADF: support for Py2.7

--- opcua/client/ua_client.py
+++ opcua/client/ua_client.py
@@ -147,7 +147,7 @@ class UASocketClient(object):
         self._do_stop = True
         try:
             self._socket.socket.shutdown(socket.SHUT_RDWR)
-        except OSError as exc:
+        except (socket.error, OSError) as exc:
             if exc.errno in (errno.ENOTCONN, errno.EBADF):
                 pass # Socket is not connected, so can't send FIN packet.
             else:
@@ -198,7 +198,7 @@ class UASocketClient(object):
             with self._lock:
                 # some servers send a response here, most do not ... so we ignore
                 future.cancel()
-        except OSError as exc:
+        except (socket.error, OSError) as exc:
             if exc.errno in (errno.ENOTCONN, errno.EBADF):
                 # Socket is closed, so can't send CloseSecureChannelRequest.
                 self.logger.warning("close_secure_channel() failed: socket already closed")
