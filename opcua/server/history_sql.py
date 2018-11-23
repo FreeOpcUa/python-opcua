0892b5ed2981bd3cf2332e6b2ffb3d831b571fd6
Volodymyr Kozachynskyi <volodymyr.kozachynskyi@tu-berlin.de>
2018-11-23 13:31:19 +0100

    now dv returns corrent timestamps

--- opcua/server/history_sql.py
+++ opcua/server/history_sql.py
@@ -116,7 +116,7 @@ class HistorySQLite(HistoryStorageInterface):
 
                     # rebuild the data value object
                     dv = ua.DataValue(variant_from_binary(Buffer(row[6])))
-                    dv.SourceTimestamp = row[1]
+                    dv.ServerTimestamp = row[1]
                     dv.SourceTimestamp = row[2]
                     dv.StatusCode = ua.StatusCode(row[3])
 
