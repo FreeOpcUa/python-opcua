d13d8a2cb4d23469350f1cb1f9670ae7ca1ba3a3
oroulet <olivier@loke>
2019-02-08 16:02:43 +0100

    fix return of nodes after delete

--- opcua/common/manage_nodes.py
+++ opcua/common/manage_nodes.py
@@ -388,7 +388,7 @@ def delete_nodes(server, nodes, recursive=False, delete_target_references=True):
         nodestodelete.append(it)
     params = ua.DeleteNodesParameters()
     params.NodesToDelete = nodestodelete
-    return params.NodesToDelete, server.delete_nodes(params)
+    return nodes, server.delete_nodes(params)
 
 
 def _add_childs(nodes):
