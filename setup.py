869e5f047c744d19ca563a1f906f5fcb55683dd2
oroulet <olivier@loke>
2019-12-03 13:50:23 +0100

    new release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ if sys.version_info[0] < 3:
     install_requires.extend(["enum34", "trollius", "futures"])
 
 setup(name="opcua",
-      version="0.98.7",
+      version="0.98.8",
       description="Pure Python OPC-UA client and server library",
       author="Olivier Roulet-Dubonnet",
       author_email="olivier.roulet@gmail.com",
