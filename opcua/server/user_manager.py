ae318501d70094e710dc2cb446e5448b34bdf673
Volodymyr Kozachynskyi <vovakozach@gmail.com>
2020-03-27 17:11:30 +0100

    FIX: Missing "cryptography" warning moved to class
    When someone imports opcua, he/she sees 3 same warnings, even in
    opcua is not used. I see the reasoning behind this feature, so I
    propose to move it in classes.

--- opcua/server/user_manager.py
+++ opcua/server/user_manager.py
@@ -7,11 +7,13 @@ use_crypto = True
 try:
     from opcua.crypto import uacrypto
 except ImportError:
-    logging.getLogger(__name__).warning("cryptography is not installed, use of crypto disabled")
     use_crypto = False
 
 
 class UserManager(object):
+
+    if use_crypto is False:
+        logging.getLogger(__name__).warning("cryptography is not installed, use of crypto disabled")
 
     class User(Enum):
         """
