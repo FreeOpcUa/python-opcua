0d2aaf62d0bef3e25105d2a28cbfb0ffb57ab03a
Andreas <info@andreas-heine.net>
2020-06-17 20:47:55 +0200

    update

--- opcua/ua/ua_binary.py
+++ opcua/ua/ua_binary.py
@@ -140,6 +140,9 @@ class _Primitive1(object):
     def pack_array(self, data):
         if data is None:
             return Primitives.Int32.pack(-1)
+        if not isinstance(data, list):
+            logger.warning(f'ua_binary.py > _Primitive1 > pack_array > data: {data} is not a instance of "list"!')
+            return Primitives.Int32.pack(-1) #to prevent crashing while runtime
         sizedata = Primitives.Int32.pack(len(data))
         return sizedata + struct.pack(self._fmt.format(len(data)), *data)
 
