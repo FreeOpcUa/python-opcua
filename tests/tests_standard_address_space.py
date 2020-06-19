2eeb47f7b0fdc0db22bec57b0ef807ec65056b1b
Andreas <info@andreas-heine.net>
2020-06-19 22:19:50 +0200

    Update tests_standard_address_space.py

--- tests/tests_standard_address_space.py
+++ tests/tests_standard_address_space.py
@@ -1,3 +1,4 @@
+import pytest
 import unittest
 import os.path
 import xml.etree.ElementTree as ET
@@ -53,7 +54,7 @@ class StandardAddressSpaceTests(unittest.TestCase):
         self.aspace = AddressSpace()
         self.node_mgt_service = NodeManagementService(self.aspace)
         standard_address_space.fill_address_space(self.node_mgt_service)
-        
+
     @pytest.mark.skip("Donot understand that code and I am not sure we should test the xml file. it is not ours")
     def test_std_address_space_references(self):
         std_nodes = read_nodes(os.path.abspath(os.path.join(os.path.dirname(__file__), '..', 'schemas', 'Opc.Ua.NodeSet2.xml')))
